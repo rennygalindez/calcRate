e2c4d50eaae513838225fdfb2333ee948b1d7573
Renny Galindez <rfgalindez@gmail.com>
2020-07-11 20:36:00 -0500

    add some comments to handler control funtion new

--- src/controllers/trades.js
+++ src/controllers/trades.js
@@ -3,9 +3,12 @@ const rates = require('../helpers/get_base_rates');
 const ratesCalc = require('../helpers/exchage_calc.js');
 
 module.exports = {
+  // Asynchronous handler controller function
   new: async (req, res) => {
     try {
+      // get offers and BTC/USD price.
       let { advs_buy, advs_sell, xbx_price } = await rates.getRates();
+      // With offers and BTC/USD price, ratesCalc makes the many comparitions
       let calculations = await ratesCalc(advs_buy, advs_sell, xbx_price);
       res.render('trades/test', { calculations });
     } catch (error) {
