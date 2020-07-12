5f55803c3b4a5f81eb70f23c837ec9a12fcd6f40
Renny Galindez <rfgalindez@gmail.com>
2020-07-11 19:58:26 -0500

    My trades controller put together my helpers functions (exchange_calc and get_base_rates)

--- src/controllers/trades.js
+++ src/controllers/trades.js
@@ -1,8 +1,16 @@
 // Import the models if it's necesary
+const rates = require('../helpers/get_base_rates');
+const ratesCalc = require('../helpers/exchage_calc.js');
 
 module.exports = {
-  new: (req, res) => {
-    res.render('users/new');
+  new: async (req, res) => {
+    try {
+      let { advs_buy, advs_sell, xbx_price } = await rates.getRates();
+      let calculations = await ratesCalc(advs_buy, advs_sell, xbx_price);
+      res.render('trades/test', { calculations });
+    } catch (error) {
+      console.log(error);
+    }
   },
   processNew: (req, res) => {},
 };
