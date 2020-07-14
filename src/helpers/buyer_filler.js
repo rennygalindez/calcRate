9ea54d736245c26d81733121965a39ff567db907
Renny Galindez <rfgalindez@gmail.com>
2020-07-14 15:48:27 -0500

    Improve returned obj to make easy the read.

--- src/helpers/buyer_filler.js
+++ src/helpers/buyer_filler.js
@@ -1,17 +1,18 @@
 //listo
 
 module.exports = function (adv_buyer, exchange_rate_to_bsf) {
-  console.log(exchange_rate_to_bsf);
+  let link = adv_buyer.actions.public_view;
+  let { username } = { ...adv_buyer.data.profile };
+  let { trade_count, feedback_score, bank_name, min_amount } = {
+    ...adv_buyer.data,
+  };
   return {
-    name: adv_buyer.data.profile.username,
-    trade_count: adv_buyer.data.profile.trade_count,
-    feedback_score: adv_buyer.data.profile.feedback_score,
-    public_view: adv_buyer.actions.public_view,
-    bank_name: adv_buyer.data.bank_name,
-    min_amount: new Intl.NumberFormat('ve-VE', {
-      style: 'currency',
-      currency: 'VEN',
-    }).format(adv_buyer.data.min_amount),
+    username,
+    trade_count,
+    feedback_score,
+    bank_name,
+    min_amount,
     exchange_rate_to_bsf,
+    link,
   };
 };
--- src/helpers/seller_filler.js
+++ src/helpers/seller_filler.js
@@ -1,15 +1,21 @@
 //listo
 
 module.exports = function (adv_sell) {
+  let link = adv_sell.actions.public_view;
+  let { username } = { ...adv_sell.data.profile };
+  let { trade_count, feedback_score, bank_name, min_amount } = {
+    ...adv_sell.data,
+  };
   return {
-    name: adv_sell.data.profile.username,
-    trade_count: adv_sell.data.profile.trade_count,
-    feedback_score: adv_sell.data.profile.feedback_score,
-    public_view: adv_sell.actions.public_view,
-    bank_name: adv_sell.data.bank_name,
-    min_amount: new Intl.NumberFormat('co-CO', {
-      style: 'currency',
-      currency: 'COP',
-    }).format(adv_sell.data.min_amount),
+    username,
+    trade_count,
+    feedback_score,
+    bank_name,
+    min_amount,
+    link,
   };
 };
+// new Intl.NumberFormat('co-CO', {
+//   style: 'currency',
+//   currency: 'COP',
+// }).format(adv_sell.data.min_amount),
