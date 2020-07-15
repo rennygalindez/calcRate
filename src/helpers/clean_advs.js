4f703ad6662f1d4d4a5250701a736b8c77554b5a
Renny Galindez <rfgalindez@gmail.com>
2020-07-15 12:04:08 -0500

    Change  to make corresponding with the new filtered structure

---
+++ src/helpers/clean_advs.js
@@ -0,0 +1,27 @@
+// These functions get arrays of seller_adv and buy_adv and filter they and fusion the data in just one structure (data +  data.profile + actions --> data)
+
+let cleanAndFusion = function () {
+  let cleanerAdvs = arguments[0]
+    .filter((e) => e.data.temp_price !== e.data.temp_price_usd) //remove advertisements with same value in temp_price and temp_price_usd, because it cause erros in rates calculations.
+    .map((e) => Object.assign(e.data, e.actions, e.data.profile)); // fusion the objects "data" "data.profile" and "actions", inside just "data" (data + data.profile + acions --> data)
+  return cleanerAdvs;
+};
+
+module.exports = function () {
+  let dirtyAdvs = [...arguments];
+  let cleanedAdvs = [];
+  dirtyAdvs.forEach((e) => {
+    cleanedAdvs.push(cleanAndFusion(e));
+  });
+  console.log(cleanedAdvs);
+  return cleanedAdvs;
+};
+
+let otherOption = function () {
+  let cleanedAdvs = [];
+  let { ...args } = { ...arguments[0] };
+  for (key in args) {
+    cleanedAdvs.push(cleanAndFusion(args[key]));
+  }
+  return cleanedAdvs;
+};
--- src/helpers/get_exchance_rate_cop_to_bsf.js
+++ src/helpers/get_exchance_rate_cop_to_bsf.js
@@ -1,8 +1,8 @@
 // Function to calculate cop to bsf rate, using www.tradeblock.com BTC/USD rate.
 
 module.exports = function (adv_sell, adv_buy, xbx_price) {
-  let usd_seller = adv_sell.data.temp_price / xbx_price;
-  let usd_buyer = adv_buy.data.temp_price / xbx_price;
+  let usd_seller = adv_sell.temp_price / xbx_price;
+  let usd_buyer = adv_buy.temp_price / xbx_price;
   let exchange_rate_to_bsf = usd_seller / usd_buyer;
   return exchange_rate_to_bsf;
 };
