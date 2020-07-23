a3a9546758f55b8ff4739cb40032ff0fb5dd8e01
Renny Galindez <rfgalindez@gmail.com>
2020-07-22 19:43:01 -0500

    Change sequelize config and add cert

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,14 @@
 // New config to sequelize, it add process.env support bisede to a new
 
+const fs = require('fs');
+const path = require('path');
+
+//
+
+CERT = fs
+  .readFileSync(path.join(__dirname, './cert/rds-ca-2019-root.pem'))
+  .toString();
+
 module.exports = {
   development: {
     username: null,
@@ -10,11 +19,17 @@ module.exports = {
     storage: '/home/renny/nodeProjects/own/calcRate/src/developmentDB.sqlite3',
   },
   test: {
-    username: process.env.USER_NAME,
-    password: '',
-    database: 'divisappTEST',
-    host: '',
+    username: process.env.TEST_USER_NAME,
+    password: process.env.TEST_PASSWORD,
+    database: process.env.TEST_PRODUCTION_DB,
+    host: process.env.TEST_DB_PRODUCTION_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+        ca: CERT,
+      },
+    },
   },
   production: {
     username: process.env.USER_NAME,
@@ -22,5 +37,12 @@ module.exports = {
     database: process.env.PRODUCTION_DB,
     host: process.env.DB_PRODUCTION_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+        ca: CERT,
+        connectionTimeoutMillis: 10,
+      },
+    },
   },
 };
