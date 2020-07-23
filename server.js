cdc00f3d63191d32547286f756f668c047e68ae9
Renny Galindez <rfgalindez@gmail.com>
2020-07-22 23:22:47 -0500

    Session and passport configs in server.js

--- server.js
+++ server.js
@@ -2,25 +2,44 @@ const express = require('express');
 const app = express();
 const pug = require('pug');
 const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./src/config/passport/passportConfig');
+passportConfig(passport);
+
+//
+
 //Settings
 
 app.set('PORT', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'pug');
 
+//
+
 //Middlewares
-app.use(express.urlencoded());
+
 app.use(express.static(path.join(__dirname, 'src', 'public')));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+  }),
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//
 
 //Routes
 
 app.use(require('./src/routes/trades_routes'));
 app.use(require('./src/routes/index_routes'));
 
-
-
 //Server running
 
 app.listen(app.get('PORT'), '0.0.0.0', () =>
-  console.log(`server running on port ${app.get('PORT')}`)
+  console.log(`server running on port ${app.get('PORT')}`),
 );
